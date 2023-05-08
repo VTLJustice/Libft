d2c06e278ce5d28990e925f7667d91f0e74227de
VTLJustice <r.n.r.2386@gmail.com>
2023-05-08 16:05:16 +0200

    Add files via upload

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rradules <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 15:36:55 by rradules          #+#    #+#             */
+/*   Updated: 2023/05/08 16:03:43 by rradules         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*first;
+
+	first = malloc(sizeof(t_list));
+	if (!first)
+		return (NULL);
+	first->content = content;
+	first->next = NULL;
+	return (first);
+}
